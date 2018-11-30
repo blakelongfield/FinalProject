545e25646179e68a8e1814742724ae246b325be3
z-lamb <lamb.zachary@gmail.com>
2018-11-30 13:29:03 -0700

    trail entity

--- FinalProjectJPA/src/main/java/com/skilldistillery/skireport/entities/Trail.java
+++ FinalProjectJPA/src/main/java/com/skilldistillery/skireport/entities/Trail.java
@@ -1,5 +1,119 @@
 package com.skilldistillery.skireport.entities;
 
-public class Trail {
+import java.util.List;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+
+@Entity
+public class Trail {
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private int id;
+	private String name;
+	private Difficulty difficulty;
+	private Integer length;
+	@Column(name="elevation_gain_loss")
+	private Integer elevationGainLoss;
+	private String features;
+	@ManyToMany
+	private List<ChairLift> lifts;
+	
+	/*
+	 * getters / setters
+	 */
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public Difficulty getDifficulty() {
+		return difficulty;
+	}
+	public void setDifficulty(Difficulty difficulty) {
+		this.difficulty = difficulty;
+	}
+	public Integer getLength() {
+		return length;
+	}
+	public void setLength(Integer length) {
+		this.length = length;
+	}
+	public Integer getElevationGainLoss() {
+		return elevationGainLoss;
+	}
+	public void setElevationGainLoss(Integer elevationGainLoss) {
+		this.elevationGainLoss = elevationGainLoss;
+	}
+	public String getFeatures() {
+		return features;
+	}
+	public void setFeatures(String features) {
+		this.features = features;
+	}
+	
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + id;
+		return result;
+	}
+	
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Trail other = (Trail) obj;
+		if (id != other.id)
+			return false;
+		return true;
+	}
+	
+	
+	
+	@Override
+	public String toString() {
+		StringBuilder builder = new StringBuilder();
+		builder.append("Trail [id=").append(id)
+				.append(", name=").append(name)
+				.append(", difficulty=").append(difficulty)
+				.append(", length=").append(length)
+				.append(", elevationGainLoss=").append(elevationGainLoss)
+				.append(", features=").append(features)
+				.append("]");
+		return builder.toString();
+	}
+	
+	public Trail() {
+		super();
+	}
+	
+	public Trail(int id, String name, Difficulty difficulty, Integer length, Integer elevationGainLoss,
+			String features) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.difficulty = difficulty;
+		this.length = length;
+		this.elevationGainLoss = elevationGainLoss;
+		this.features = features;
+	}
+	
 }
