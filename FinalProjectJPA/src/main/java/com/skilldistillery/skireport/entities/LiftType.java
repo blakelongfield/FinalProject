92e8db5e76da406e9ae5e4a030a7f54af669b1a7
papadini <krpaladini@gmail.com>
2018-11-30 13:36:58 -0700

    lyfttype and chairlift entites

---
+++ FinalProjectJPA/src/main/java/com/skilldistillery/skireport/entities/LiftType.java
@@ -0,0 +1,119 @@
+package com.skilldistillery.skireport.entities;
+
+import java.util.List;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+
+
+@Table(name="'chairlift_type")
+public class LiftType {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	
+	private String type;
+
+	private Integer capacity;
+	
+	@OneToMany(mappedBy="type")
+	private List<ChairLift> lifts;
+	
+	
+	// GETTERS AND SETTER
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
+
+	public Integer getCapacity() {
+		return capacity;
+	}
+
+	public void setCapacity(Integer capacity) {
+		this.capacity = capacity;
+	}
+
+	public List<ChairLift> getLifts() {
+		return lifts;
+	}
+
+	public void setLifts(List<ChairLift> lifts) {
+		this.lifts = lifts;
+	}
+
+	
+	// HASH CODE AND EQUALS
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + id;
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		LiftType other = (LiftType) obj;
+		if (id != other.id)
+			return false;
+		return true;
+	}
+
+	
+	// TO STRING
+	
+	
+	
+	
+	@Override
+	public String toString() {
+		StringBuilder builder = new StringBuilder();
+		builder.append("LiftType [id=");
+		builder.append(id);
+		builder.append(", type=");
+		builder.append(type);
+		builder.append(", capacity=");
+		builder.append(capacity);
+		builder.append(", lifts=");
+		builder.append(lifts);
+		builder.append("]");
+		return builder.toString();
+	}
+	
+	
+	// CONSTRUCTORS
+	public LiftType(int id, String type, Integer capacity, List<ChairLift> lifts) {
+		super();
+		this.id = id;
+		this.type = type;
+		this.capacity = capacity;
+		this.lifts = lifts;
+	}
+	
+
+	public LiftType() {}
+}
---
+++ FinalProjectJPA/src/test/java/com/skilldistillery/skireport/entities/ChairliftTest.java
@@ -0,0 +1,52 @@
+package com.skilldistillery.skireport.entities;
+
+import static org.junit.Assert.assertEquals;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class ChairliftTest {
+	
+	private static EntityManagerFactory emf;
+	private EntityManager em;
+
+	@BeforeAll
+	public static void setupALL() {
+		emf = Persistence.createEntityManagerFactory("SkiReport");
+	}
+
+	@AfterAll
+	public static void tearDownALL() {
+		emf.close();
+	}
+
+	@BeforeEach
+	public void setUpBeforeClass() throws Exception {
+		em = emf.createEntityManager();
+	}
+
+	@AfterEach
+	public void tearDownAfterClass() throws Exception {
+		em.close();
+
+	}
+
+	@Test
+	@DisplayName("testing connection to data base")
+	void test() {
+		
+		ChairLift cl = em.find(ChairLift.class, 1);
+		
+		assertEquals(1, cl.getType().getType());
+		
+	}
+
+}
