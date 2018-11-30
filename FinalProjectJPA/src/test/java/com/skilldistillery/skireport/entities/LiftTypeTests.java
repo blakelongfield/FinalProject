3921e28017e910df7e783e3760eb47507a010d41
Blake Longfield <blakelongfield@gmail.com>
2018-11-30 16:00:04 -0700

    finished tests

---
+++ FinalProjectJPA/src/test/java/com/skilldistillery/skireport/entities/LiftTypeTests.java
@@ -0,0 +1,47 @@
+package com.skilldistillery.skireport.entities;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class LiftTypeTests {
+	private static EntityManagerFactory emf;
+	EntityManager em;
+	private LiftType liftType;
+	
+	@BeforeAll
+	static void setUpBeforeClass() throws Exception{
+		emf = Persistence.createEntityManagerFactory("SkiReport");
+	}
+	
+	@BeforeEach
+	void setUp() throws Exception {
+		em = emf.createEntityManager();
+		liftType = em.find(LiftType.class, 1);
+	}
+	
+	@Test
+	void test1() {
+		assertEquals(4, liftType.getCapacity().intValue());
+		assertEquals("Express", liftType.getType());
+		assertEquals(1, liftType.getLifts().size());
+	}
+	
+	@AfterAll
+	static void tearDownAfterClasS() {
+		emf.close();
+	}
+
+	@AfterEach
+	void tearDown() throws Exception {
+		em.close();
+	}
+}
