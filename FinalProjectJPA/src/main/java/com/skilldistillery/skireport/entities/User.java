00b93b6d5047d92a8c6e7249a9da4fa371e3fa5e
Tyler Paladini <TylerPaladini@yahoo.com>
2018-11-30 12:40:17 -0700

    User and Comment started

--- FinalProjectJPA/src/main/java/com/skilldistillery/skireport/entities/User.java
+++ FinalProjectJPA/src/main/java/com/skilldistillery/skireport/entities/User.java
@@ -1,5 +1,25 @@
 package com.skilldistillery.skireport.entities;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class User {
-	// Kyle is so stupid
+	
+	@Id
+	@GeneratedValue(strategy= GenerationType.IDENTITY)
+	private int id;
+	
+	private String firstName;
+	private String lastName;
+	private String userName;
+	private String password;
+	private String email;
+	private String role;
+	private Boolean active;
+	private String imgUrl;
+		
+	
 }
