5f29cb11aa6c121a81550e1c6d0c9918f3d2d6d2
z-lamb <lamb.zachary@gmail.com>
2018-12-03 13:15:58 -0700

    updated routing component

--- Angular/skireview/src/app/app-routing.module.ts
+++ Angular/skireview/src/app/app-routing.module.ts
@@ -1,7 +1,26 @@
+import { AdminComponent } from './admin/admin.component';
+import { LogoutComponent } from './logout/logout.component';
+import { LoginComponent } from './login/login.component';
+import { HomeComponent } from './home/home.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { TrailDetailsComponent } from './trail-details/trail-details.component';
+import { ProfileComponent } from './profile/profile.component';
+import { RegisterComponent } from './register/register.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: 'home' },
+  { path: 'home', component: HomeComponent },
+  { path: 'trail', component: TrailDetailsComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'logout', component: LogoutComponent },
+  { path: 'profile', component: ProfileComponent },
+  { path: 'register', component: RegisterComponent },
+  { path: 'admin', component: AdminComponent }
+  // { path: 'resort', component: ResortCompent }
+
+  // { path: '**', component: NotFoundComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
