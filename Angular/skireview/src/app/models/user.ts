dc0a41e0851d310d37b1f13bdb24e81572c39953
Blake Longfield <blakelongfield@gmail.com>
2018-12-03 10:38:38 -0700

    added models to angular

--- Angular/skireview/src/app/models/user.ts
+++ Angular/skireview/src/app/models/user.ts
@@ -1,2 +1,25 @@
 export class User {
+  id: number;
+  firstName: string;
+  lastName: string;
+  username: string;
+  password: string;
+  email: string;
+  role: string;
+  active: boolean;
+  profilePic: string;
+
+  // tslint:disable-next-line:max-line-length
+  constructor(id?: number, firstName?: string, lastName?: string, username?: string, password?: string, email?: string, role?: string, active?: boolean, profilePic?: string) {
+    this.id = id;
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.username = username;
+    this.password = password;
+    this.email = email;
+    this.role = role;
+    this.active = active;
+    this.profilePic = profilePic;
+  }
+
 }
