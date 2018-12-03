e805919613364322e97e1f82f9b6c1c86b55455b
Tyler Paladini <TylerPaladini@yahoo.com>
2018-12-03 11:56:40 -0700

    update to user service

--- Angular/skireview/src/app/register/register.component.ts
+++ Angular/skireview/src/app/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { User } from '../models/user';
 
 @Component({
   selector: 'app-register',
@@ -6,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit {
+
+  newuser: User = new User();
 
   constructor() { }
 
