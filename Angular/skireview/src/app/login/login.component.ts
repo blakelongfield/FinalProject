cfbc0294dce7a4920c72a01d253c4144199d1509
Tyler Paladini <TylerPaladini@yahoo.com>
2018-12-04 09:16:02 -0700

    stubbed out login

--- Angular/skireview/src/app/login/login.component.ts
+++ Angular/skireview/src/app/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { User } from '../models/user';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +8,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
+  loginUser: User = new User();
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  public login() {
+
   }
 
 }
