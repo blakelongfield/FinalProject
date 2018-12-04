04b7c1125aeefe7261b461b4b309f2067b8b7fa4
papadini <krpaladini@gmail.com>
2018-12-04 09:40:09 -0700

    gome page updates

--- FinalProjectJPA/src/main/java/com/skilldistillery/skireport/entities/Trail.java
+++ FinalProjectJPA/src/main/java/com/skilldistillery/skireport/entities/Trail.java
@@ -52,7 +52,7 @@ public class Trail {
 	inverseJoinColumns=@JoinColumn(name="chairlift_id"))
 	private List<ChairLift> lifts;
 	
-//	@JsonIgnore
+	@JsonIgnore
 	@OneToMany(mappedBy="trail")
 	private List<Report> reports;
 
